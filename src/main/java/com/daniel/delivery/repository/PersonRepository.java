411b17b65a9e3c1cc16bc34a4539f04aebb0b73e
DanielGerasimenko <danielgerasimenko@gmail.com>
2022-06-06 16:19:32 +0300

    Changes

--- src/main/java/com/daniel/delivery/repository/PersonRepository.java
+++ src/main/java/com/daniel/delivery/repository/PersonRepository.java
@@ -4,6 +4,4 @@ import com.daniel.delivery.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
-
 }
